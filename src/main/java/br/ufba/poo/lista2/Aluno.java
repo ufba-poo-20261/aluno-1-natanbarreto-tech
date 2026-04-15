e7a3eb2ccbf3bed2db3895a0cafc56ecb944c07a
natanbarreto-tech <natanbarreto.eng@gmail.com>
2026-04-14 23:10:54 -0300

    Atividade2ClasseAluno.java

--- src/main/java/br/ufba/poo/lista2/Aluno.java
+++ src/main/java/br/ufba/poo/lista2/Aluno.java
@@ -1,4 +1,13 @@
+/*
+ * Aluno: Natan Barreto de Souza Alves
+ * Professor: Rodrigo Rocha Gomes e Souza
+ * Disciplina: MATA55 - Programação Orientada a Objetos
+ * Data: 14/04/2026
+ */
+
 package br.ufba.poo.lista2;
+
+import java.util.Objects;
 
 /**
  * Crie a classe `Aluno`, que possui os atributos `matricula` e `nome` (ambos do
@@ -16,7 +25,40 @@ package br.ufba.poo.lista2;
  */
 public class Aluno {
 
+    private final String matricula;
+    private String nome;
+
     public Aluno(String matricula, String nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Aluno)) {
+            return false;
+        }
+        Aluno outro = (Aluno) obj;
+        return Objects.equals(matricula, outro.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matricula);
     }
 }
-
